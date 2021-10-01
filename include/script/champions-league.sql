3cb48d4de3cf613835e379215c45aead66333fe6
carlsmithetienne2000@gmail.com <carlsmithetienne2000@gmail.com>
2021-10-01 01:15:59 -0400

    Modification Script SQL

--- include/script/champions-league.sql
+++ include/script/champions-league.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `champions-league`
 --
-
+CREATE DATABASE `champions-league`;
+USE `champions-league`;
 -- --------------------------------------------------------
 
 --
