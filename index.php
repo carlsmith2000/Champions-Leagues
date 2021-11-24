cc7acb71eb5a64c052880236fa1b3b1845617fc5
Carl Smith <carlsmithetienne2000@gmail.com>
2021-11-24 01:01:15 -0500

    penalite

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-
-<?php
-require_once('./classes/database/Database.class.php');
-require_once('./classes/equipes/TeamModel.class.php');
-require_once('./classes/equipes/TeamView.class.php');
-
-$imagesLink = './css/images/';
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -19,6 +7,7 @@ $imagesLink = './css/images/';
 </head>
 
 <body>
+
     <section id="accueil">
         <h1>COUPE INUKA</h1>
         <!-- <img src="./css/images/world-cup.png" alt="World Cup Trophy"> -->
