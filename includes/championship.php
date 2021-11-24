9126068710be8da3242b247bad23e970f5d1d734
Wenchy Price Cadet <tchyprice2013@gmail.com>
2021-11-24 02:57:33 -0500

    coupe Inuka

--- includes/championship.php
+++ includes/championship.php
@@ -21,7 +21,7 @@ $gameControlleur = new GameController();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link type="text/css" rel="stylesheet" href="../css/championship.css">
-    <title>Calendrier | Coupe 3ème Infos</title>
+    <title>Calendrier | Coupe Inuka</title>
 </head>
 
 <body>
